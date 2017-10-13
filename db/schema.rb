206602ecdb52221b62925c38d31929a0bb62e89b
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-13 14:32:41 +0900

    dbやtest類の先程の実装に対応化

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012221113) do
+ActiveRecord::Schema.define(version: 20171013032510) do
+
+  create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name", null: false
+    t.string "image"
+    t.integer "price"
+    t.string "purchase_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email"
