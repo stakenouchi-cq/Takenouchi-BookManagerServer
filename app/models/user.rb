b3ffb460450f3b9e66bfab92d8cf789333c25136
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-13 14:28:35 +0900

    書籍クラスを作成し、ユーザと書籍を1対多の関係にした

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	has_many :books
+
 	before_save {self.email = email.downcase}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
