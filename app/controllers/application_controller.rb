e118a3ee4eef2c9a9d4f7f7583ac59829d0846c9
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-13 12:13:02 +0900

    ログイン機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::API
 		render json: {status: 200, result: UserSerializer.new(user)}, status: :ok
 	end
 
-	def failed_sign_up(user)
+	def success_login(user)
+		render json: {status: 200, result: UserSerializer.new(user)}, status: :ok
+	end
+
+	def failed_request(user)
 		render json: {status: 401, message: "Request Error"}, status: :unprocessable_entity
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
+  
   def sign_up
     user = User.new(user_params)
     if user.save!
       success_sign_up(user)
     else
-      failed_sign_up(user)
+      failed_request(user)
+    end
+  end
+
+  def login
+    user = User.find_by(email: user_params[:email])
+    # URLパラメータにあるpasswordを直接使う必要があるため、paramsにする 
+    if user && user.authenticate(params[:password])
+      success_login(user)
+    else
+      failed_request(user)
     end
   end
 
@@ -12,4 +23,5 @@ class UsersController < ApplicationController
     def user_params
       params.permit(:email, :password)
     end
+
 end
