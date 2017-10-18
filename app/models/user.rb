59af18f5bfa3627ca7689a4af357f3299d548f26
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-18 17:44:20 +0900

    リダイレクトの無効化と、ログアウトされているアカウントにログインされたら新しいトークンを生成するよう修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,25 @@
 class User < ApplicationRecord
   # Include default devise modules.
+  has_many :books
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-	has_many :books
+          :recoverable, :rememberable, :trackable, :validatable
 
 	before_save {self.email = email.downcase}
-	after_create :update_access_token!
+	after_create :update_token
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
   			uniqueness: {case_sensitive: false}, length: {maximum: 50}
   validates :password, presence: true, length: {minimum: 6}
-  # has_secure_password
-  # has_secure_token :token
 
-  def update_access_token!
-    self.token = "#{self.id}:#{Devise.friendly_token}"
-    save
+  def update_token
+    # アカウント新規作成時やログアウト後によりトークンがnullであれば、新たなトークンを生成
+    self.update_attribute(:token, "#{ self.token || Devise.friendly_token }")
+  end
+
+  def trash_token
+    # ログアウト時にトークンを破棄
+    self.update(token: nil)
   end
 
 end
