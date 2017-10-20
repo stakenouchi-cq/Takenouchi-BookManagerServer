cff1f1c4fa8d8e8f2acd017a75f792247445fa4c
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-20 12:11:30 +0900

    imgurへアップロード時の例外処理を追加

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,16 +1,21 @@
 require 'httpclient'
 
 module ImageHelper
-  URL = 'https://api.imgur.com/3/image'
+  URL = 'https://api.imgur.com/3/image'.freeze
 
   def upload_to_imgur(image_string)
-    http_client = HTTPClient.new
-    auth_header = { 'Authorization' => 'Client-ID 61073815e3d76a2' }
-    parameters = { :image => image_string, :type => 'base64' }
-    res = http_client.post(URI.parse(URL), parameters, auth_header)
-
-    result_hash = JSON.load(res.body)
-    return result_hash['data']['link']
+    begin
+      http_client = HTTPClient.new
+      auth_header = { 'Authorization' => 'Client-ID 61073815e3d76a2' }
+      parameters = { :image => image_string, :type => 'base64' }
+      res = http_client.post(URI.parse(URL), parameters, auth_header)
+      result_hash = JSON.load(res.body)
+      result_hash['data']['link']
+    rescue HTTPClient::BadResponseError, HTTPClient::TimeoutError => e
+      puts("HTTPClientError: #{e}")
+    rescue JSON::ParserError => e
+      puts("JSON ParserError: #{e}")
+    end
   end
 
 end
