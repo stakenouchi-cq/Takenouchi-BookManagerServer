cd99832e4337b356700d662a9726445cde75dd9f
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-20 09:50:45 +0900

    書籍データ作成用のmigrateファイルを修正

--- db/migrate/20171013032510_create_books.rb
+++ db/migrate/20171013032510_create_books.rb
@@ -1,12 +1,11 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
-      t.references :user #, foreign_key: true
-      t.string :name, :null => false
+      t.references :user
+      t.string :name, null: => false
       t.string :image
       t.integer :price
       t.string :purchase_date
-      # t.integer :user_id, :null => false
 
       t.timestamps
     end
