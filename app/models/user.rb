b26769c69bf8ede91fbfb7a768fc649c5b090ecf
Shotaro Takenouchi <s.takenouchi@caraquri.com>
2017-10-24 10:03:14 +0900

    トークンの文字数を30字に変更。また、ユーザーのmodel内から、メールアドレスやパスワードの有効性チェックを削除 (config/initializers/devise.rbのファイル内で同様のチェックを行っているため)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,11 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   after_create :update_token
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
-        uniqueness: { case_sensitive: false }, length: { maximum: 50 }
-  validates :password, presence: true, length: { minimum: 6 }
-
   def update_token
     # アカウント新規作成時やログアウト後によりトークンがnullであれば、新たなトークンを生成
-    self.update_attribute(:token, "#{ self.token || Devise.friendly_token }")
+    unless self.update( token: self.token || SecureRandom.urlsafe_base64(n=30) )
+      render_internal_server_error # サーバー内部のエラーでトークン更新失敗    
+    end
   end
 
   def trash_token
